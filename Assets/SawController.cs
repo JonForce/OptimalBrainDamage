c679d08883de68434da07021e0f2ad8b17aceb3a
Noah.D.Lape <noah.d.lape@gmail.com>
2020-11-01 15:27:00 -0500

    working on saw

---
+++ Assets/SawController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SawController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody sawBlade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sawBlade = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sawBlade.maxAngularVelocity = speed;
+        sawBlade.AddTorque(transform.forward);
+    }
+}
